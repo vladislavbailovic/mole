7a4017a99de0bb1a6daedac33d3faefd6e8e8a2f
vladislav <ve@incsub.com>
2022-12-24 09:00:10 +0100

    Temporarily showcase multiple jobs

--- main.go
+++ main.go
@@ -5,23 +5,33 @@ import (
 	"fmt"
 	"mole/internal"
 	"sync"
+	"time"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	job := internal.NewJob(
+	job1 := internal.NewLimitedJob(
 		"testdata/**/*.txt",
 		internal.NewCommand("ls", []string{"-la"}),
+		time.Second*24,
 	)
+	job2 := internal.NewLimitedJob(
+		"testdata/nested",
+		internal.NewCommand("ls", []string{"-la"}),
+		time.Second*2,
+	)
+	jobs := []*internal.Job{job1, job2}
 
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go (func(job *internal.Job) {
-		job.Watch(ctx)
-		wg.Done()
-	})(job)
+	for _, j := range jobs {
+		wg.Add(1)
+		go (func(job *internal.Job) {
+			job.Watch(ctx)
+			wg.Done()
+		})(j)
+	}
 
 	wg.Wait()
 	fmt.Println("-- all done --")
